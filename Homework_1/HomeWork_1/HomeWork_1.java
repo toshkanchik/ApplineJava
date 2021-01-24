b6c7d2f18f86a9ecb44e0f46b7e629a15430cfb9
toshkanchik <lukyanov.anton.vladimirovich@gmail.com>
2021-01-24 18:48:38 +0300

    Added comments

--- Homework_1/HomeWork_1/HomeWork_1.java
+++ Homework_1/HomeWork_1/HomeWork_1.java
@@ -10,14 +10,28 @@ public class HomeWork_1 {
 		box1.addItem(sweet1);
 		sweet1 = new Chocolate("zxcv",5,6);
 		box1.addItem(sweet1);
-		box1.printInfo();
-		box1.removeItem(0);
-		box1.printInfo();
+		box1.printInfo(); //test addItem + print
+		
+		
+		box1.removeItem(10);
+		box1.printInfo(); //test remove index out of bounds
+		
+		box1.removeItem(1);
+		box1.printInfo(); //test remove index
+		
+		box1.removeItem();
+		box1.printInfo(); //test remove last
+		
 		sweet1 = new BubbleGum("zaq",7,8,"mint");
 		box1.addItem(sweet1);
 		box1.printInfo();
 		box1.printPrice();
-		box1.printWeight();
+		box1.printWeight(); //test print total
+		
+		box1.removeItem();
+		box1.removeItem();
+		box1.removeItem();
+		box1.printInfo(); //test remove from empty box
 	}
 
 }
