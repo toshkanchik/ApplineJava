6f09232ac4be6dce5826fd2dc42d3c21166a1d9c
toshkanchik <lukyanov.anton.vladimirovich@gmail.com>
2021-01-25 14:40:06 +0300

    Task with * is finished.

--- Homework_1/HomeWork_1/HomeWork_1.java
+++ Homework_1/HomeWork_1/HomeWork_1.java
@@ -32,6 +32,20 @@ public class HomeWork_1 {
 		box1.removeItem();
 		box1.removeItem();
 		box1.printInfo(); //test remove from empty box
+		
+		box1.addItem(new Iriska("qwer",1,6,3));
+		box1.addItem(new Iriska("asdf",6,1,3));
+		box1.addItem(new Iriska("zxcv",3,4,3));
+		box1.addItem(new Iriska("zaq",2,5,3));
+		box1.addItem(new Iriska("xsw",5,2,3));
+		box1.addItem(new Iriska("cde",4,3,3));
+		box1.printInfo();
+		box1.printWeight();
+//		box1.removeToWeightByPrice(16);//test for additional task
+//		box1.removeToWeightByWeight(16);//test for additional task
+		box1.removeToWeight(17, false);//test for additional task
+		box1.printInfo();
+		box1.printWeight();
 	}
 
 }
--- Homework_1/HomeWork_1/SweetsBox.java
+++ Homework_1/HomeWork_1/SweetsBox.java
@@ -50,22 +50,115 @@ public class SweetsBox implements PresentInt {
 		}
 		System.out.println("___________");
 	}
-
-	@Override
-	public void printWeight() {
-		int total = 0;
-		for(int i = 0; i < currIndex; i++) {
-			total += arr[i].getWeight();
-		}
-		System.out.println("Total weight is:" + total);
-	}
-
-	@Override
-	public void printPrice() {
+	
+	public int getTotalPrice() {
 		int total = 0;
 		for(int i = 0; i < currIndex; i++) {
 			total += arr[i].getPrice();
 		}
-		System.out.println("Total price is:" + total);
+		return total; 
+	}
+	
+	public int getTotalWeight() {
+		int total = 0;
+		for(int i = 0; i < currIndex; i++) {
+			total += arr[i].getWeight();
+		}
+		return total; 
+	}
+
+	@Override
+	public void printWeight() {
+//		int total = 0;
+//		for(int i = 0; i < currIndex; i++) {
+//			total += arr[i].getWeight();
+//		}
+		System.out.println("Total weight is:" + getTotalWeight());
+	}
+
+	@Override
+	public void printPrice() {
+//		int total = 0;
+//		for(int i = 0; i < currIndex; i++) {
+//			total += arr[i].getPrice();
+//		}
+		System.out.println("Total price is:" + getTotalPrice());
+	}
+	
+	private int getMinPriceIndex() {//gets index of item with lowest price. -1 in case of error
+		int minIndex = -1;
+		int minPrice = Integer.MAX_VALUE;
+		for(int i = 0; i < currIndex; i++) {
+			if(arr[i].getPrice() <= minPrice) {
+				minIndex = i;
+				minPrice = arr[i].getPrice(); 
+			}
+		}
+		return minIndex;
+	}
+	
+	private int getMinWeightIndex() {//gets index of item with lowest weight. -1 in case of error
+		int minIndex = -1;
+		int minWeight = Integer.MAX_VALUE;
+		for(int i = 0; i < currIndex; i++) {
+			if(arr[i].getWeight() <= minWeight) {
+				minIndex = i;
+				minWeight = arr[i].getWeight(); 
+			}
+		}
+		return minIndex;
+	}
+	
+	public int removeToWeightByPrice(int targetWeight) {//returns -1 if removing is not possible, returns new total weight otherwise (can remove all items if targetWeight is too low)
+		int total = this.getTotalWeight();
+		if (targetWeight >= total) {
+			return total;
+		}
+		else if ((targetWeight <= 0) || (currIndex == 0)) {
+			return -1;
+		}
+		else {
+			int remove = getMinPriceIndex();
+			while ((remove >= 0) && (getTotalWeight() > targetWeight)) {
+				this.removeItem(remove);
+				remove = getMinPriceIndex();
+			}
+			return getTotalPrice();
+		}
+	}
+	public int removeToWeightByWeight(int targetWeight) {//returns -1 if removing is not possible, returns new total weight otherwise (can remove all items if targetWeight is too low)
+		int total = this.getTotalWeight();
+		if (targetWeight >= total) {
+			return total;
+		}
+		else if ((targetWeight <= 0) || (currIndex == 0)) {
+			return -1;
+		}
+		else {
+			int remove = getMinWeightIndex();
+			while ((remove >= 0) && (getTotalWeight() > targetWeight)) {
+				this.removeItem(remove);
+				remove = getMinWeightIndex();
+			}
+			return getTotalPrice();
+		}
+	}
+	
+	public int removeToWeight(int targetWeight, boolean usePrice) {//This is only one method, but it is more rational use it instead of two above
+		int total = this.getTotalWeight();
+		if (targetWeight >= total) {
+			return total;
+		}
+		else if ((targetWeight <= 0) || (currIndex == 0)) {
+			return -1;
+		}
+		else {
+			int remove = (usePrice) ? getMinPriceIndex() : getMinWeightIndex();
+			while ((remove >= 0) && (getTotalWeight() > targetWeight)) {
+				this.removeItem(remove);
+				remove = (usePrice) ? getMinPriceIndex() : getMinWeightIndex();
+			}
+			return getTotalPrice();
+		}
 	}
 }
