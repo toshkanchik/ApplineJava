bf201d71628ecf9e5d3f865c9f4801cf3242339d
toshkanchik <lukyanov.anton.vladimirovich@gmail.com>
2021-01-28 01:41:40 +0300

    open file feature added.

--- Homework_2/src/Homework_2/WordCounter.java
+++ Homework_2/src/Homework_2/WordCounter.java
@@ -2,15 +2,41 @@ package Homework_2;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class WordCounter {
     public static void main(String[] args) {
-        //D:\StudyAppline\ApplineJava\Homework_2\InputFile.txt
-        File myFile = new File("D://StudyAppline//ApplineJava//Homework_2//InputFile.txt");
+        Path currentRelativePath = Paths.get("");
+        String currPath = currentRelativePath.toAbsolutePath().toString();
+        System.out.print("Current working path is: " + currPath + "\nEnter full or relative InputFilePath: ");
+        Scanner sc = new Scanner(System.in);
+        String filePath = sc.nextLine();
+        File enteredPathFile = null;
+        try{
+            while (true){
+                enteredPathFile = new File(currPath + "//" + filePath);
+                if(enteredPathFile.exists()){
+                    break;
+                }
+                else {
+                    enteredPathFile = new File(filePath);
+                    if(enteredPathFile.exists()){
+                        break;
+                    }
+                }
+                System.out.print("No such file. Enter full or relative InputFilePath: ");
+                filePath = sc.nextLine();
+            }
+
+        }catch (NullPointerException e){
+            e.printStackTrace();
+        }
+
         Scanner scanner = null;
         try {
-            scanner = new Scanner(myFile);
+            scanner = new Scanner(enteredPathFile);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             scanner.close();
