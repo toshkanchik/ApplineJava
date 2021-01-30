6ca52e9849c28f412c877416859e136e0048dc75
toshkanchik <lukyanov.anton.vladimirovich@gmail.com>
2021-01-30 03:19:51 +0300

    +3 methods: printCompList printAssetsList getYoungerThan

--- Homework_3/src/main/java/companiesViewer/companiesViewer.java
+++ Homework_3/src/main/java/companiesViewer/companiesViewer.java
@@ -20,45 +20,116 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 import companiesViewer.company.*;
-
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.lang.reflect.Type;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class companiesViewer {
     public static LinkedList<Company> companies = new LinkedList<>();
-//    public static SimpleDateFormat formatter = new SimpleDateFormat("yyyy-dd-MM");
     public static SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yy");
 
 
     public static void main(String[] args) {
         final Type REVIEW_TYPE = new TypeToken<LinkedList<Company>>() {
         }.getType();
-//        Gson gson = new Gson();
         Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
         JsonReader reader;
         try {
-//            reader = new JsonReader(new FileReader("D:\\StudyAppline\\ApplineJava\\Homework_3\\src\\main\\resources\\inputFile.json"));
             reader = new JsonReader(new FileReader("src\\main\\resources\\inputFile.json"));
             companies = gson.fromJson(reader, REVIEW_TYPE); // contains the whole reviews list
 //            printAll();
             printCompList();
+            printAssetsList();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        getYoungerThan();
     }
 
-    public static void printCompList(){ //Вывести все имеющиеся компании в формате «Краткое название» – «Дата основания 17/01/98»;
+
+//    Вывести все имеющиеся компании в формате «Краткое название» – «Дата основания 17/01/98»;
+    public static void printCompList(){
         for (Company currComp : companies){
             System.out.println(currComp.getName() + " - " + formatter.format(currComp.getFoundationDate()));
         }
     }
 
-    public static void printAssetsList(){
-        //ToDo
+
+//    Вывести все ценные бумаги (их код, дату истечения и полное название организации-владельца),
+//    которые просрочены на текущий день, а также посчитать суммарное число всех таких бумаг;
+    public static void printAssetsList() {
+        long totalExpires = 0;
+        for (Company comp : companies) {
+            totalExpires = totalExpires + comp.getAssets().stream()
+                    .filter(asset -> {
+                        if (asset.getDate().before(new Date())) {
+                            System.out.printf("ID: %s  expired: %s  belongs to: %s%n", asset.getId(), formatter.format(asset.getDate()), comp.getName());
+                            return true;
+                        } else return false;
+                    }).count();
+        }
+        System.out.println("Total expired: " + totalExpires);
     }
+
+
+
+
+//    На запрос пользователя в виде даты «ДД.ММ.ГГГГ», «ДД.ММ.ГГ», «ДД/ММ/ГГГГ» и «ДД/ММ/ГГ» вывести
+//    название и дату создания всех организаций, основанных после введенной даты;
+    public static List<Company> getYoungerThan(){
+        Scanner scanner = new Scanner(System.in);
+        SimpleDateFormat formatter1 = new SimpleDateFormat("dd.MM.yy");
+        SimpleDateFormat formatter2 = new SimpleDateFormat("dd/MM/yy");
+        String strDate;
+        Date date = new Date();
+        boolean validDate = false;
+        while (!validDate){
+            System.out.println("Please input date: ");
+            strDate = scanner.nextLine();
+            try {
+                date = formatter1.parse(strDate);
+                validDate = true;
+            } catch (java.text.ParseException e){validDate = false;}
+            if(!validDate) {
+                try {
+                    date = formatter2.parse(strDate);
+                    validDate = true;
+                } catch (java.text.ParseException e) {
+                    validDate = false;
+                }
+            }
+        System.out.println(date);
+        }
+        final Date fDate = date;
+        return companies.stream().filter(company -> {
+            if (company.getFoundationDate().after(fDate)){
+                System.out.printf("%s  %s \n", company.getName(), formatter.format(company.getFoundationDate()));
+                return true;
+            } else return false;
+        }).collect(Collectors.toList());
+
+
+
+
+    }
+
+
+
+//    На запрос пользователя в виде кода валюты, например EU, USD, RUB и пр. выводить id и коды ценных
+//    бумаг, использующих заданную валюту.
+    public static void getByCurrency(){
+
+    }
+
+//        compan.stream().forEach(company -> {
+//            System.out.println(company.getName());
+//        });
 
     public static void printAll(){
         //ToDo Comment this method
